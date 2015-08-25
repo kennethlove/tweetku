866977f8d0fdd967b435ebcd556363d416c8b8bf
Kenneth Love <kenneth@gigantuan.net>
2015-08-24 17:12:23 -0700

    Smarter csv building

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import itertools
 import random
 import re
 
+from nltk.corpus import wordnet
+
 word_syllables = re.compile(
     r'^(?P<word>[-\w]+)\s{2}(?P<syllables>(\w+\d?\s?)+)$',
 )
@@ -17,13 +19,11 @@ def build_csv(corpus):
     for line in itertools.filterfalse(lambda x: x.startswith(';;;'),
                                       open(corpus, 'r', encoding='utf-8')):
         match = word_syllables.match(line)
-        if match and len(match.group('syllables').split()) < 8:
+        if match and all([
+            len(match.group('syllables').split()) < 8,
+            wordnet.synsets(match.group('word'))
+        ]):
             yield match.group('word'), len(match.group('syllables').split())
-
-
-with open('words.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(build_csv('dict.txt'))
 
 
 def build_dict(csvfile):
@@ -36,10 +36,8 @@ def build_dict(csvfile):
     return word_dict
 
 
-words = build_dict('words.csv')
-
-
 def get_line(syllables):
+    words = build_dict('words.csv')
     total = 0
     order = []
     while total < syllables:
@@ -48,3 +46,10 @@ def get_line(syllables):
         total += num
     line = map(lambda l: random.choice(words[l]), order)
     return list(line)
+
+
+if __name__ == '__main__':
+    with open('words.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(build_csv('dict.txt'))
+
